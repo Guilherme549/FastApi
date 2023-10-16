a449625b6e8c2eb4a901fc61c2a43f0aa888ca74
Guilherme Francisco Alves Ribeiro <101433358+Guilherme549@users.noreply.github.com>
2023-10-15 22:57:02 -0300

    Login

--- Fast APi/main.py
+++ Fast APi/main.py
@@ -25,4 +25,26 @@ def cadastro(nome: str, user: str, senha: str):
         return {"status": "sucesso"}
     elif len(usuario) > 0:
         return {"status": "usuario ja cadastrado"}
-      
+
+
+@app.post("/login")
+def login(usuario: str, senha: str):
+    session = conectaBanco()
+    user = session.query(Pessoa).filter_by(usuario=usuario, senha=senha).all()
+    if len(user) == 0:
+        return {"status": "usuario nao existe"}
+
+    while True:
+        token = token_hex(50)
+        tokenExiste = session.query(Tokens).filter_by(token=token).all()
+        if len(tokenExiste) == 0:
+            pessoaExiste = session.query(Tokens).filter_by(id=user[0].id).all()
+            if len(pessoaExiste) == 0:
+                novoToken = Tokens(id_pessoa=user[0].id, token=token)
+                session.add(novoToken)
+
+            elif len(pessoaExiste) > 0:
+                pessoaExiste[0].token = token
+
+            session.commit()
+        return token
